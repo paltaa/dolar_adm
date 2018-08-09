7f18e7f01f2eef5b3ba3f7e726af9c1af3efb786
Juan Quiroga <jquiroga1991@gmail.com>
2018-08-08 22:52:12 -0400

    scrapeado y guardado

--- dolar/urls.py
+++ dolar/urls.py
@@ -2,11 +2,13 @@ from django.urls import path, include
 from . import  views
 from rest_framework import routers
 
+#import scraper
+
+
 router = routers.DefaultRouter()
 
 router.register('dolar', views.DolarView)
 
 urlpatterns = [
 path('', include(router.urls)),
-
 ]
--- scraper.py
+++ scraper.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup
 import urllib.request
 import pandas as pd
-
+from dateutil import parser
+from dolar.models import Dolar
 
 def html_to_df( table ):
     new_table = pd.DataFrame(columns=range(0,13), index= range(0,32))
@@ -15,31 +16,39 @@ def html_to_df( table ):
         row_marker += 1
         if(row_marker==32):
             break
-
-
     return new_table
 
-years = [
-"2013",
-"2014",
-"2015",
-"2016",
-"2017",
-"2018"
-]
-html_doc= "http://www.sii.cl/valores_y_fechas/dolar/dolar2015.htm#"
-html_docb= "http://www.sii.cl/pagina/valores/dolar/dolar2010.htm"
-url = urllib.request.urlopen(html_doc)
-urlb= urllib.request.urlopen(html_docb)
-with url as fp:
-    soup = BeautifulSoup(fp)
-with urlb as fp:
-    soupb = BeautifulSoup(fp)
 
-table = soup.find("div", {"id": "mes_all"}, {"id": "table export"})
-tableb= soupb.find("div", {"id": "contenido"})
+def scraper(year):
+    year_str=str(year)
+    if ( year >=  2013):
+        html_doc= "http://www.sii.cl/valores_y_fechas/dolar/dolar"+year_str+".htm#"
+    elif(year >= 1990 and year < 2013):
+        html_doc= "http://www.sii.cl/pagina/valores/dolar/dolar"+year_str+".htm"
+    url = urllib.request.urlopen(html_doc)
+    with url as fp:
+        soup = BeautifulSoup(fp)
+    if ( year >=  2013):
+        table = soup.find("div", {"id": "mes_all"}, {"id": "table export"})
+    elif(year >= 1990 and year < 2013):
+        table= soup.find("div", {"id": "contenido"})
+    new_table = html_to_df( table )
+    days , months = new_table.shape
+    dolarByDate=list()
+    for month in range(1,months):
+        for day in range(1,days):
+            value=new_table.iloc[day][month].replace(',','.')
+            if(value != '' and value != '>\xa0' and value !='\xa0' ):
+                date=parser.parse(year_str+'-'+str(month)+'-'+str(day))
+                print(value)
+                value=float(value)
+                dolarByDate.append((date, value))
+    return dolarByDate
 
+for year in range(1991,2019):
+    dolar_year=scraper(year)
+    for tuples in dolar_year:
+        dolar=Dolar(date=tuples[0], value=tuples[1])
 
-new_table = html_to_df( tableb )
-
-print (new_table)
+        print ( tuples )
+        print(dolar.save())
