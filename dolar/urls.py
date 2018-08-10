986c8b23b0986b8beb55682dcc16a13a881af622
Juan Quiroga <jquiroga1991@gmail.com>
2018-08-09 22:33:12 -0400

    final commit
    todo funcionando

--- dolar/urls.py
+++ dolar/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path, include
 from . import  views
 from rest_framework import routers
+from django.conf.urls import url
 
 #import scraper
 
 
 router = routers.DefaultRouter()
 
-router.register('dolar', views.DolarView)
+#router.register('dolar', views.DolarView)
+#router.register('dolar', views.dolar)
 
 urlpatterns = [
 path('', include(router.urls)),
+url('usd', views.usd),
+url('clp', views.clp)
 ]
--- dolar/views.py
+++ dolar/views.py
@@ -2,10 +2,38 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Dolar
 from .serializers import DolarSerializer
-
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
 
 
-class DolarView(viewsets.ModelViewSet):
-    queryset = Dolar.objects.all()
-    serializer_class = DolarSerializer
+@api_view(['GET'])
+
+def usd(request):
+    date = request.GET.get('date')
+    clp= float(request.GET.get('clp'))
+    try:
+        dlr = Dolar.objects.get(date=date)
+    except Dolar.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        #serializer = DolarSerializer(dlr)
+        response=clp/float(dlr.value)
+        #return Response(serializer.data)
+        return Response("el valor en dólares "+ str(response)+" del monto en pesos "+str(clp)+", en la fecha "+ str(date))
+@api_view(['GET'])
+
+def clp(request):
+    date = request.GET.get('date')
+    usd = float(request.GET.get('usd'))
+    try:
+        dlr = Dolar.objects.get(date=date)
+    except Dolar.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        response=usd*float(dlr.value)
+        #return Response(serializer.data)
+        return Response("el valor en clp "+str(response)+"del monto en dolares "+str(usd)+", en la fecha"+ str(date))
